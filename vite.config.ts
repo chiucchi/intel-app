41f322570ad87acce8b2b7dbe1712d7078da77b5
chiucchi <gchiucchi01@gmail.com>
2023-02-24 22:54:08 -0300

    feat: add beautiful imports mode

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+        '@': '/src',
+        '@components': '/src/components',
+        '@pages': '/src/pages',
+        '@utils': '/src/utils',
+        '@assets': '/src/assets',
+        '@tokens': '/src/tokens',
+        '@hooks': '/src/hooks',
+        '@context': '/src/context',
+    }
+  }
 })
